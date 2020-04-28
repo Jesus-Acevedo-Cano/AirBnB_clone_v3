d2297080099b4787b35860402c83a8cc06505419
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-04-28 05:44:43 +0000

    add stats route to index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,17 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status')
 def status():
     """ return status """
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats")
+def objs_counter():
+    """return total count of each obj per class"""
+    classes = ["Amenity", "City", "Place", "Review", "State", "User"]
+    classes_dic = {}
+    for i in classes:
+        classes_dic[i] = storage.count(eval(i))
+    return jsonify(classes_dic)
